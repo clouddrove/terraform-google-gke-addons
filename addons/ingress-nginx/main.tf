9903446a41ea7098f4f355f1555d44fb342d9f59
Tanveer <ertanveer143@gmail.com>
2024-01-11 22:22:14 +0530

    feat: remove unneccessary data from main.tf

--- addons/ingress-nginx/main.tf
+++ addons/ingress-nginx/main.tf
@@ -5,23 +5,4 @@ module "helm_addon" {
   environment = var.environment
   addon_context = var.addon_context
 
-  set_values = [
-    {
-      name  = "gcpRegion"
-      value = "us-central1"
-    },
-    {
-      name  = "autoDiscovery.clusterName"
-      value = var.gke_cluster_name
-    },
-    {
-      name  = "rbac.serviceAccount.create"
-      value = "false"
-    },
-    {
-      name  = "rbac.serviceAccount.name"
-      value = "${local.name}-sa"
-    }
-  ]
-
 }
