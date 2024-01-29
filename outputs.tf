86ba3f8a6e6527606970a9247bfa9f06bcb82199
Tanveer <ertanveer143@gmail.com>
2024-01-29 22:36:54 +0530

    feat: Updated output.tf

--- outputs.tf
+++ outputs.tf
@@ -67,3 +67,17 @@ output "keda_repository" {
   value       = module.keda[*].repository
   description = "helm repository url of keda"
 }
+
+#-----------Kubeclarity-------------------
+output "namespace" {
+  value = local.default_helm_config.namespace
+}
+
+output "chart_version" {
+  value = local.default_helm_config.version
+}
+
+output "repository" {
+  value = local.default_helm_config.repository
+}
+
